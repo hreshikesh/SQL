3652419338d364feb9f7da08b9754e3f53cd8756
Hreshikesh <ailhreshikesh@gmail.com>
2025-04-19 20:54:55 +0530

    Create and Alter

--- Create and Alter.sql
+++ Create and Alter.sql
@@ -1,1 +1,18 @@
 create database Task;
+
+use Task;
+
+create table movie_info(
+id int,
+movie_name varchar(10),
+movie_ratings int(5),
+movie_director varchar(20),
+movie_lang varchar(10),
+movie_producer varchar(10),
+movie_collection bigint,
+movie_actoe varchar(10),
+movie_actress varchar(20),
+movie_villian varchar(20)
+
+);
+

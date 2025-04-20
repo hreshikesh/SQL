d65b897966b3ed1cfdf6186dedb41b3867e749a1
Hreshikesh <ailhreshikesh@gmail.com>
2025-04-20 22:28:37 +0530

    Create and Alter

--- Create and Alter.sql
+++ Create and Alter.sql
@@ -16,3 +16,43 @@ movie_villian varchar(20)
 
 );
 
+CREATE TABLE Payments (
+    payment_id INT PRIMARY KEY,
+    order_id INT,
+    customer_id INT,
+    payment_date DATE,
+    amount_paid DECIMAL(10, 2),
+    payment_method VARCHAR(50),
+    transaction_id VARCHAR(100),
+    payment_status VARCHAR(50),
+    billing_address VARCHAR(255),
+    currency VARCHAR(10)
+);
+
+CREATE TABLE BankAccounts (
+    account_id INT PRIMARY KEY,
+    account_holder_name VARCHAR(100),
+    account_type VARCHAR(50),
+    account_number VARCHAR(20),
+    ifsc_code VARCHAR(20),
+    balance DECIMAL(15, 2),
+    branch_name VARCHAR(100),
+    opened_date DATE,
+    status VARCHAR(20),
+    currency VARCHAR(10)
+);
+CREATE TABLE InstaUsers (
+    user_id INT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE,
+    full_name VARCHAR(100),
+    email VARCHAR(100),
+    phone_number VARCHAR(15),
+    password_hash VARCHAR(255),
+    bio TEXT,
+    profile_pic_url VARCHAR(255),
+    join_date DATE,
+    is_verified VARCHAR(5)
+);
+
+
+
